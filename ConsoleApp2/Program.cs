ebddca3abbf66d456dbdcb510f460cdf23bf074c
igorp <io006235@cfjl.com.br>
2024-11-21 21:41:34 -0300

    Git init

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp2
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            const string url = "https://jsonplaceholder.typicode.com/posts";
+            await Get(url);
+            await Post(url);
+        }
+        
+        static async Task Post(string url)
+        {
+            var client = new HttpClient();
+            var response = await client.PostAsync(url, new StringContent("{'title':'foo','body':'bar','userId':1}"));
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+        }
+        
+        static async Task Get(string url)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+        }
+    }
+}
